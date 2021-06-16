2b86cac2c0f621885302c8132bcb66da1c45deae
Aaron Deas <deas.aaron@gmail.com>
2021-06-15 22:35:35 -0400

    Added more questions to inquirer prompt

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-console.log("Hello");
-const inquirer = require("inquirer");
-const inquire = require("inquirer");
-const menuChoices = require("./questions");
+console.log('Hello');
+const inquirer = require('inquirer');
+const inquire = require('inquirer');
+const {menuChoices, addEmployee, removeEmployee } = require('./questions');
 
 inquirer.prompt(menuChoices).then((res) => {
-	switch (res.menuChoice) {
-		case "View All Employees":
-			console.log("Menu choice view all employees");
-	}
+  switch (res.menuChoice) {
+    case 'Add Employee':
+	  inquirer.prompt(addEmployee).then((res) => {
+		  console.log(res);
+	  })
+      break;
+    case 'Remove Employee':
+		inquirer.prompt(removeEmployee).then((res) => {
+			console.log(res);
+		})
+      break;
+    case 'Update Employee Role':
+      break;
+    case 'Update Employee Manager':
+      break;
+    case 'Add Role':
+      break;
+	  default: 
+	  console.log('error')
+  }
 });
--- questions.js
+++ questions.js
@@ -1,21 +1,38 @@
 const menuChoices = [
-	{
-		type: "list",
-		name: "menuChoice",
-		message: "What would you like to do?",
-		choices: [
-			"View All Employees",
-			"View all Employees by Department",
-			"View all Employees by Manager",
-			"Add Employee",
-			"Remove Employe",
-			"Update Employee Role",
-			"Update Employee Manager",
-			"View All Roles",
-			"Add Role",
-			"Remove Role",
-		],
-	},
+  {
+    type: 'list',
+    name: 'menuChoice',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Employees',
+      'View all Employees by Department',
+      'View all Employees by Manager',
+      'Add Employee',
+      'Remove Employee',
+      'Update Employee Role',
+      'Update Employee Manager',
+      'View All Roles',
+      'Add Role',
+      'Remove Role',
+    ],
+  },
 ];
 
-module.exports = menuChoices;
+const addEmployee = [
+  {
+    type: 'input',
+    name: 'employeeType',
+    message: 'Please enter the type of employee you would like to add',
+    default: 'Regular',
+  },
+];
+
+const removeEmployee = [
+  {
+    type: 'input',
+    name: 'employeeToRemove',
+    message: 'Please select the Employee you wish to remove',
+  },
+];
+
+module.exports = { menuChoices, addEmployee, removeEmployee };
